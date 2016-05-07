3734b56a75dcc3705b01b340d74a6d098325ec61
ThunderGemios10 <thundergemios10@gmail.com>
2016-05-07 15:57:45 +0800

    Rename files

--- start_server.bat
+++ start_server.bat
@@ -1,9 +1,9 @@
 @echo off
-echo [BATCH] Starting JSlither
+echo [BATCH] Starting Clither
 :loop
    cls
    npm start
    echo -------------------------------------------------------
-   echo [BATCH] JSlither Shutdown, waiting 15 seconds before a restart.
+   echo [BATCH] Clither Shutdown, waiting 15 seconds before a restart.
    timeout /t 15 > nul
    goto loop
